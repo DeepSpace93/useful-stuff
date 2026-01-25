41872b9636aec0a2678d3ebffc52efbd91bb66f2
DeepSpace93 <jonas.m93@gmx.net>
2026-01-25 19:13:24 +0100

    stuff

--- js/gcdIO.js
+++ js/gcdIO.js
@@ -3,5 +3,9 @@ function gcdIO() {
     let a = parseInt(document.getElementById("a").value);
     let b = parseInt(document.getElementById("b").value);
 
-    document.getElementById("gcd").value = gcd(a, b);
+    let gcd_res = gcd(a, b);
+
+    document.getElementById("a$gcd").value = a/gcd_res;
+    document.getElementById("b$gcd").value = b/gcd_res;
+    document.getElementById("gcd").value = gcd_res;
 }
